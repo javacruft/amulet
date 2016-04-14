732b069d156037ffa3817799a8448167a55cef7f
James Page <james.page@ubuntu.com>
2016-04-14 12:24:26 +0100

    Fixup conditional assessment for Git charms

--- amulet/charm.py
+++ amulet/charm.py
@@ -33,8 +33,8 @@ class CharmCache(dict):
                 series
             )
 
-        if (branch and branch.endswith('.git') or
-                branch.startswith('https://github.com')):
+        if (branch and (branch.endswith('.git') or
+                branch.startswith('https://github.com'))):
             return GitCharm(branch, name=charm_path)
 
         if os.path.exists(os.path.expanduser(charm_path)):
